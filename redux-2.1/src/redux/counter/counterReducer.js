71ca7362bcdd03376b26117aa52771ac3f6dfd67
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-04-11 01:19:26 +0600

    counter changed to value

--- redux-2.1/src/redux/counter/counterReducer.js
+++ redux-2.1/src/redux/counter/counterReducer.js
@@ -4,19 +4,21 @@ const initialState = {
     value: 0
 }
 
-export const counterReducer = (state = initialState, action) => {
+const counterReducer = (state = initialState, action) => {
     switch (action?.type) {
         case INCREMENT:
             return {
                 ...state,
-                counter: state.counter + action.payload
+                value: state.counter + action.payload
             }
         case DECREMENT:
             return {
                 ...state,
-                counter: state.counter - action.payload
+                value: state.counter - action.payload
             }
         default:
             return state
     }
 }
+
+export default counterReducer;
