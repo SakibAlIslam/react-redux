5cd3681983fda540f0e3ff80ecc8231ad6667db2
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-04-12 02:52:58 +0600

    combineReducers used

--- redux-2.1/src/components/HooksCounter.jsx
+++ redux-2.1/src/components/HooksCounter.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../redux/counter/actions";
 
 const HooksCounter = () => {
-  const count = useSelector((state) => state?.value);
+  const count = useSelector((state) => state?.counter?.value);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
