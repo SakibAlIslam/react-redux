d0f28f70c8aed518d08292194c541ab9dd21aaf6
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-04-11 06:41:19 +0600

    useSelector and useDispatch added and 2.6 done

--- redux-2.1/src/App.jsx
+++ redux-2.1/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Counter from "./components/Counter";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import HooksCounter from "./components/HooksCounter";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         </h1>
 
         <div class="max-w-md mx-auto mt-10 space-y-5">
-          <Counter />
+          <HooksCounter />
         </div>
       </div>
     </Provider>
